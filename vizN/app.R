255624216c04cc94fb062152a939dd08a6d9c120
Matt Wilkins <mattwilkins@MacBook-Pro.local>
2019-01-17 14:46:29 -0600

    Added fractional title, tweaked interface. Is niiice!

--- vizN/app.R
+++ vizN/app.R
@@ -13,17 +13,27 @@ library(shiny);require(ggplot2)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("So you're sayin' there's a chance?"),
+   titlePanel("vizN: visualizing numbers & fractions"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-        uiOutput("numero"),
-         uiOutput("outOf"),
-        sliderInput("pointcex","Point scale",min=1,max=20,value=1,step=.2)
+        fluidRow(
+          column(6, uiOutput("numero")
+            ),
+          column(6, uiOutput("outOf"))
+            ),
+        fluidRow(
+          column(4,
+          checkboxInput("scalepts","Autoscale Points?",value=T)),
+          column(8,
+          sliderInput("pointcex","Manual point scale",min=.1,max=20,value=3,step=.2)
+            )
+      )
       ),
+    
       
-      # Show a plot of the generated distribution
+      # Show a vizualization
       mainPanel(
          plotOutput("viz",height=600),
         verbatimTextOutput("string")
@@ -33,27 +43,22 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   # output$viz <- renderPlot({
-   #    # generate bins based on input$bins from ui.R
-   #    x
-   #    
-   #    # draw the histogram with the specified number of bins
-   #    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   # })
-   #output$string<-renderText(rep("a",input$N))
+   #Render input sliders that actually respect min/max values
    output$numero<-renderUI({
      numericInput(inputId = "k", label = "How many?", min = 1, max = 1e9,value=1)
-    # numericInput(inputID= "k2", label="How many?",min=1,max=100,value=1)
    })
    
    
    output$outOf <- renderUI({
-    numericInput(inputId = "numba", label = "Out of ____ (Enter an integer between 1 & 1 billion)", min = 1, max = 1e9,value=100) #2
+    numericInput(inputId = "numba", label = "Out of ____? (integer)", min = 1, max = 1e9,value=100) #2
     })
    
    output$viz<- renderPlot({
      N <- input$numba
+     k<- input$k
+     #Autoscale vs Manual point scaling
+     if(input$scalepts==F){pts<-input$pointcex}else{pts=20*exp(-.0025*N)+.1}
+     
      root<-sqrt(N)
      lim<-ceiling(root) #What's the nearest integer length of root N
      remainder<-ifelse(root==lim,0,(lim^2-N)) #if square axes don't equal root N, calculate how many points to remove
@@ -62,13 +67,21 @@ server <- function(input, output) {
      if(remainder!=0){ #If the grid isn't square, remove the remainder values
        remove.indx<-(nrow(d)-remainder):nrow(d)
        d<-d[-remove.indx[-1],]#remove[-1] first element bc of greedy remove.indx definition (remainder=1 should just remove 1 element)
-       }
-     g<-ggplot(d,aes(x=x,y=y))+geom_point(alpha=.3,size=input$pointcex)+theme_void()+xlab("")+ylab("")+coord_fixed()+geom_point(data=d[1:input$k,],aes(x=x,y=y),col="red",fill="red",pch=19,cex=input$pointcex*1.5)
-     if(N>200000){
+     }
+     mytheme<-theme_void()
+     g<-ggplot(d,aes(x=x,y=y))+geom_point(alpha=1,size=pts,col="#011627")+xlab("")+ylab("")+xlim(min(d$x)-1,max(d$x)+1)+ylim(min(d$y)-1,max(d$y)+1)+mytheme+ guides(fill = guide_legend(title = "LEFT", title.position = "left"))+coord_fixed()
+     #If How many>0, add red dots
+     if(k>0){g<-g+geom_point(data=d[1:k,],aes(x=x,y=y),col="#F71735",pch=19,cex=pts*1)}
+     
+     if(N>1000000){
        binmagnitude<-round(log10(N))-3
-       g <- g+stat_summary_2d()+ guides(fill = guide_legend(title = "LEFT", title.position = "left"))
-   }
-     g
+       g <- g+stat_summary_2d()
+     }
+     #Add title
+     warn<-ifelse(k>N,"  *Improper Fraction*","")
+     ttl<-substitute( paste(frac(k,N),warn),list(k=format(k, big.mark=",", scientific=FALSE),N=format(N, big.mark=",", scientific=FALSE),warn=warn))
+     
+     g+ ggtitle(ttl)+theme(plot.title = element_text(hjust = 0.5,size=34,face="bold",colour="#011627",family="serif"))
    })
 }
 
